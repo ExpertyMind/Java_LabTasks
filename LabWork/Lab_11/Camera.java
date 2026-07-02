83bda7c78f3b220e6bf4f2bb81a04456ff915146
Muhammad Saad Bilal <183986735+ExpertyMind@users.noreply.github.com>
2026-07-02 21:25:04 +0500

    Delete LabWork directory

--- LabWork/Lab_11/Camera.java
+++
@@ -1,6 +0,0 @@
-package Lab_11;
-
-public interface Camera {
-    void TakePhoto();
-    void TakeVideo();
-}
--- LabWork/Lab_11/Microphone.java
+++
@@ -1,6 +0,0 @@
-package Lab_11;
-
-public interface Microphone {
-    void StartRecording();
-    void StopRecording();
-}
